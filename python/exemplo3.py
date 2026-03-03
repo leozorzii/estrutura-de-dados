e65ea87a497fa553b91d8b01ecc7de7cc0b4e93e
Leo Zorzi <l.zorzi@ufn.edu.br>
2026-03-03 16:48:21 +0000

    Update: remoção de arquivos duplicados

--- python/exemplo3.py
+++
@@ -1,7 +0,0 @@
-from util import popularListaArquivo
-lista = []
-
-lerArquivoLista("numeros.txt", lista)
-
-for i in lista:
-    print(i)
--- python/minhaBiblioteca.py
+++
@@ -1,7 +0,0 @@
-def popular_aleatorio(lista, quantidade):
-    pass
-
-def popular_de_arquivo(lista, nome_arquivo):
-    pass
-
-def copiarListaSemDupicatas(lista_origem):
